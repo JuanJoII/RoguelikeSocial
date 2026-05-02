fc1a89b065ad14c8d71376116933cf27743be73c
StefanyLopez <141768826+StefanyLopez@users.noreply.github.com>
2026-05-01 21:40:33 -0500

    Core: define RoomData and enum structures

--- Assets/Scripts/Procedural/Core/RoomData.cs
+++ Assets/Scripts/Procedural/Core/RoomData.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-public class RoomData : MonoBehaviour
+public class RoomData
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Vector2Int gridPosition;              // Posición en grid (ej: 0,0)
+    public Vector2Int size;                     // Tamaño (ej: 20,20)
+    public RoomType roomType;                   // Normal, Boss, Treasure, etc
+    public BiomeType biomeType;                 // Stone, Ice, Fire, Forest
+    public float decorationDensityModifier = 1f; // x1 = normal, x0.5 = menos, x2 = más
+    
+    // Contenido generado
+    public List<Vector3> wallPositions = new List<Vector3>();
+    public List<DecorationInstance> decorations = new List<DecorationInstance>();
+    public List<Vector3> enemySpawnPoints = new List<Vector3>();
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public enum RoomType { Normal, Boss, Treasure, Shop }
+public enum BiomeType { Stone, Ice, Fire, Forest }
+
+public class DecorationInstance
+{
+    public string prefabName;
+    public Vector3 position;
+    public Quaternion rotation;
+    public float scale = 1f;
 }
